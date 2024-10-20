0897a8581041a1cdcb12dd9dcefbd03e2bbaa76f
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-10-20 11:39:47 +0300

    solution: DcВсё работает // Сохраняюсь.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -8,6 +8,10 @@ import web.model.Car;
 import web.service.CarService;
 import web.util.CarUtils;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping(value = "/cars")
@@ -49,6 +53,14 @@ public class CarController {
         service.delete(id);
         return "redirect:/cars";
     }
+    @PostMapping("/delete_all")
+    public String deleteAllCars() {
+//        service.getAll().forEach(car -> service.delete(car.getId())); // так нельзя, возникает 'ConcurrentModificationException' т.к. в этом случае будет попытка модифицировать коллекцию во время её итерации через метод forEach, который не поддерживает изменение исходной коллекции
+        List<Car> deleteCarsList = new ArrayList<>(service.getAll());
+        deleteCarsList.forEach(car -> service.delete(car.getId()));
+        return "redirect:/cars";
+    }
+
 
     @GetMapping("/edit")
     public String showEditCarPage(@RequestParam("id_edited_car") Integer id, Model model) {
@@ -61,7 +73,27 @@ public class CarController {
         return "redirect:/cars";
     }
 
-@PostMapping("/generate")
+
+    @GetMapping("/create")
+    public String showCreateCarPage(Model model) {
+        Car defaultCar = CarUtils.generateCar();
+        defaultCar.setId(service.getAll().size() + 1);
+        defaultCar.setModel("Default MODEL // " + defaultCar.getModel());
+        defaultCar.setColor("Default COLOR // " + defaultCar.getColor());
+        defaultCar.setReleaseDate(new Date(System.currentTimeMillis()));
+
+        model.addAttribute("created_car", defaultCar);
+        System.out.println("defaultCar = " + defaultCar);
+        return "add_car_page";
+    }
+    @PostMapping("/create")
+    public String createCar(@ModelAttribute("created_car") Car car) {
+        service.save(car);
+        return "redirect:/cars";
+    }
+
+
+    @PostMapping("/generate")
     public String generateCar(@ModelAttribute("car_count") Integer count) {
         CarUtils.generateCar(count).stream().forEach(car -> service.save(car));
         return "redirect:/cars";
