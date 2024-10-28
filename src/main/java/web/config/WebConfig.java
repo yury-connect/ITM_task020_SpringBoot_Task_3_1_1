0e3d5fba68e234b205782c964c7e0ce770282ed1
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-10-28 04:26:41 +0300

    solution_boot: save EУбрал костыли в java-коде и НАЛО ЗАПУСКАТЬСЯ // теперь уонфигурация полностью загружается из .property файлов и настройки не конфликтуют более!!!

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -28,48 +28,13 @@ public class WebConfig implements WebMvcConfigurer {
         this.applicationContext = applicationContext;
     }
 
-
-    // Теперь настройки для Thymeleaf считываются из application.properties, поэтому конфигурация упрощена.
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        /*SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        // Эти параметры уже настроены в application.properties
-        templateResolver.setCharacterEncoding("UTF-8");*/
-
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("classpath:/templates/users_pages/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML");
-        templateResolver.setCacheable(false);
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
-
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        resolver.setCharacterEncoding("UTF-8"); // Поддержка UTF-8, чтобы русские символы отображались корректно - БЕЗ ЭТОЙ СТРОЧКИ РУССКИЕ БУКВЫ НЕ ОТОБРАЖАЮТСЯ В БРАУЗЕРЕ !!!
-        registry.viewResolver(resolver);
-    }
-
-
     // Это я подключил для того, чтобы были доступны статические ресурсы, в частности css стили
-    @Override
+    /*@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
-    }
+    }*/
 
     // Этот код включает поддержку методов DELETE, PUT и PATCH через скрытое поле _method в форме.
 //    @Bean
